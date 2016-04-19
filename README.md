fb8a2662cf90737a6a9ea9ac3f0f7c0fe90667fd
fanpaa <fanpaa@gmail.com>
2016-04-19 22:38:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+## SNH48新官方商城抢票脚本
+
+#### 脚本功能
+
+设置选票参数，加快排队速度
+
+#### 使用方法
+
+1. Chrome用户安装 [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=zh-cn) 扩展；Firefox用户安装 [Greasemonkey](https://addons.mozilla.org/zh-cn/firefox/addon/greasemonkey) 扩展
+2. 点击 [这里](https://github.com/saintwinkle/SNH48-Get-Ticket/raw/master/SNH48-Get-Ticket.user.js) 安装脚本
+3. 打开 [剧场门票](http://shop.48.cn/tickets) ，点击票务链接即可
+
+#### 更新日志
+
+[2016.04.19] 第一版，未经测试
+
+#### LICENSE
+
+MIT
---
+++ SNH48-Get-Ticket-For-48cn.user.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         SNH48 Get Ticket for 48.cn
+// @namespace    https://github.com/fanpaa/SNH48-Get-Ticket-For-48cn
+// @version      0.1
+// @description  SNH48新官方商城抢票脚本
+// @author       fanpaa
+// @match        http://shop.48.cn/tickets/item/*
+// @grant        none
+// ==/UserScript==
+
+$(function() {
+    'use strict';
+
+    console.info('===>link start!');
+
+    //设置购买数量
+    var _num=1;
+    //设置购买座位  2vip 3普 4站
+    var _seattype=3;
+    //设置循环间隔
+    var _time=2000;
+
+
+
+    var _url= "/TOrder/add";
+    var _id=location.pathname.split('/tickets/item/')[1];
+
+    function _loop(){
+        layer.msg('抢票中 0rz...');
+        setTimeout(function(){tickets();},_time);
+    }
+
+
+    function tickets(){
+        var __id =_id;
+        var __url= "/TOrder/tickCheck";
+        $.ajax({
+            url: __url,
+            type: "post",
+            dataType: "json",
+            data: { id: __id,r: Math.random() },
+            success: function (result) {
+                if (result.HasError) {
+                    _loop();
+                }
+                else
+                {
+                    switch(result.ErrorCode)
+                    {
+                        case "success":
+                            window.location.href = result.ReturnObject;
+                            break;
+                        case "fail":
+                            layer.msg(result.Message);
+                            break;                  
+                        default:
+                            _loop();
+                    }
+
+                }
+            },
+            error: function (e) {
+                _loop();
+
+            }
+        });
+    }
+
+    $.ajax({
+        url: _url,
+        type: "post",
+        dataType: "json",
+        data: { id: _id, num: _num, seattype:_seattype, r: Math.random() },
+        success: function (result) {
+            if (result.HasError) {
+                //失败操作
+                layer.msg(result.Message);
+            }
+            else {
+                if(result.Message =="success")
+                {
+                    window.location.href = result.ReturnObject;
+                }else
+                {
+                    _loop();
+                }
+            }
+        },
+        error: function (e) {
+            layer.msg("下单异常,请刷新重试");
+        }
+    });
+
+    console.info('===>link block!');
+});
